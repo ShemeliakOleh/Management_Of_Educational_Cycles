41a642b4509716fec8dee8a4f848a664b200562b
ShemeliakOleh <shemeliak.oleh.main@gmail.com>
2021-05-02 20:52:23 +0300

    Add Department, DIscipline, Faculty, Group, Teacher models.

---
+++ Management_Of_Educational_Cycles/Management_Of_Educational_Cycles.Domain/Models/Faculty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management_Of_Educational_Cycles.Domain.Models
+{
+    class Faculty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Faculty()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
