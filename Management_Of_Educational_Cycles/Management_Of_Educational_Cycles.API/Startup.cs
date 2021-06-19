4eafcff637e21ac51092904f9ade634c38af7c15
ShemeliakOleh <shemeliak.oleh.main@gmail.com>
2021-06-19 09:20:09 +0300

    Add IAcademicRepository service to Startup.

--- Management_Of_Educational_Cycles/Management_Of_Educational_Cycles.API/Startup.cs
+++ Management_Of_Educational_Cycles/Management_Of_Educational_Cycles.API/Startup.cs
@@ -43,7 +43,7 @@ namespace Management_Of_Educational_Cycles.API
             services.AddTransient<ITeachersRepository, TeachersRepository>();
             services.AddTransient<IFacultiesRepository, FacultiesRepository>();
             services.AddTransient<IDepartmentRepository, DepartmentRepository>();
-            services.AddTransient<IGroupRepository, GroupRepository>();
+            services.AddTransient<IAcademicGroupRepository, AcademicGroupRepository>();
             services.AddTransient<IDisciplineRepository, DisciplineRepository>();
             services.AddTransient<DataManager>();
         }
